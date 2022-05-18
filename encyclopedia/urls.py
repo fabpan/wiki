ee04c296b7769979eec1e24d0e97d0552ba7306a
Fabrizio <86876376+fabpan@users.noreply.github.com>
2022-05-18 18:42:42 +0200

    Riordino views

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("_newpage", views.newPage, name="newPage"),
-    path("_search", views.display, name="search"),
+    path("_search", views.search, name="search"),
     path("<str:title>", views.display, name="display"),
-
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,53 +2,73 @@ from django.shortcuts import render
 from .util import get_entry, list_entries
 from markdown2 import markdown
 from django import forms
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+
+def show_entry(request, title, entry=None):
+    if not entry:
+        entry = get_entry(title)
+    return render(request, "encyclopedia/display.html", {
+        "entry": markdown(entry), 'title': title})
+
+
+def show_list(request, entries_list, header):
+    print("SHOW LIST 01")
+    return render(request, "encyclopedia/index.html", {
+        "entries": entries_list, "header": header
+    })
 
 
 def index(request):
-        return render(request, "encyclopedia/index.html", {
-        "entries": list_entries(), "header": "All Pages"
-        })
+    # url incoming: /wiki/
+    print('INDEX 01')
+    return show_list(request, entries_list=list_entries(), header="All Pages")
 
 
-def display(request, title=None):
-    if request.method == "GET":
-        entries = list_entries()
-        q = request.GET.get("q")
-        if q:
-            if q.upper() in [entry.upper() for entry in entries]:
-                title = q
+def search(request):
+    # user asks for a search
+    q = request.GET.get("q")
+    entries = list_entries()
+    if q:
+        # not empty target url incoming /_search/?q=... not empty
+        if q.upper() in [entry.upper() for entry in entries]:
+            # found full match entry , show page of the entry
+            return show_entry(request, title=q)
+        else:
+            # look for partial matching entries/y
+            entries_list = [s for s in entries if q.upper() in s.upper()]
+            case = len(entries_list)
+            if case == 0:
+                # no partial matching found
+                header = "No result found for " + q
             else:
-                if q:
-                    list_submatch = [s for s in entries if q.upper() in s.upper()]
-                    case = len(list_submatch)
-                    if case == 0:
-                        header = "No results found for "+q
-                    else:
-                        header = q+" may refer to:"
-                else:
-                    header = "Pages available:"
-                    list_submatch = entries
-
-                return render(request, "encyclopedia/index.html", {
-                    "entries": list_submatch, "header": header
-                })
+                # found one or more partial matching
+                header = q + " may refer to:"
+            return show_list(request, entries_list, header)
+    else:
+        # empty search
+        header = "Pages available:"
+        entries_list = entries
+        return show_list(request, entries_list, header)
 
 
+def display(request, title):
+    print("DISPLAY 01")
+    # user asks for a page by its title
     entry = get_entry(title)
     if entry:
-        return render(request, "encyclopedia/display.html", {
-            "entry": markdown(entry), 'title': title})
+        # asked page exists and its content is shown
+        return show_entry(request, title, entry)
     else:
-        return render(request, "encyclopedia/display.html", {
-            "entry": "<h1> '" + title + "' non trovato</h1>", 'title': title})
+        # asked page does not exist
+        return show_entry(request, title="Page not found", entry=" ")
 
 
 class NewPageForm(forms.Form):
     newEntry = forms.CharField(label="New Entry")
     newEntryText = forms.CharField(label="New Entry Text", widget=forms.Textarea())
 
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
 
 def newPage(request):
     if request.method == "POST":
@@ -64,5 +84,3 @@ def newPage(request):
                 "entry": markdown(content), 'title': newEntrySubtd})
 
     return render(request, "encyclopedia/newpage.html", {"newPageForm": NewPageForm()})
-
-
